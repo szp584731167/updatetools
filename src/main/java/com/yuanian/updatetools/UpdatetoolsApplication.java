3fff3a2cf12d371f84f4e0fa4527c837a497a0f1
71015600  <6tSqh2t3>
2020-03-30 11:00:52 +0800

    初始文件夹

--- src/main/java/com/yuanian/updatetools/UpdatetoolsApplication.java
+++ src/main/java/com/yuanian/updatetools/UpdatetoolsApplication.java
@@ -1,9 +1,16 @@
 package com.yuanian.updatetools;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+/**spring boot会默认加载org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration类
+因为新建的spring boot 工程中没有dataSource相关的配置信息，所以一启动就报错
+ 添加(exclude={DataSourceAutoConfiguration.class})
+或者配置datasource信息，补全配置文件即可
+*/
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class UpdatetoolsApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/yuanian/updatetools/UpdatetoolsApplicationTests.java
+++ src/test/java/com/yuanian/updatetools/UpdatetoolsApplicationTests.java
@@ -1,13 +1,24 @@
 package com.yuanian.updatetools;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootTest(classes = UpdatetoolsApplication.class)
+@RunWith(SpringRunner.class)
 class UpdatetoolsApplicationTests {
+
+
 
     @Test
     void contextLoads() {
+
     }
 
 }
